3442f8cdfb6ce1f89034b1492512544f22a854fc
Dragos Busuioc <dragosbusuioc94@gmail.com>
2018-04-19 08:38:03 +0000

    refactoring details.js

--- jsorg/js/details.js
+++ jsorg/js/details.js
@@ -3,15 +3,6 @@ const Details = (function() {
 	const items = select("[rel='js-items']");
 	const detailPage = select("[rel='js-details']");
 	
-	click(items, e=>{
-		e.preventDefault();
-		e.stopPropagation();
-		let t = e.target;
-		if(t.classList.contains("item")) {
-			get(t);
-		}
-	});
-	
 	const get = t => {
 		let url = t.getAttribute("rel");
 		url = url[url.length-1];
@@ -20,4 +11,19 @@ const Details = (function() {
 		return fetch(`./details/${url}`).then(res=>res.text()).then(text=>detailPage.innerHTML = text);
 	};
 	
+	const init = () => {
+		click(items, e=>{
+			e.preventDefault();
+			e.stopPropagation();
+			let t = e.target;
+			if(t.classList.contains("item")) {
+				get(t);
+			}
+		});
+	};
+	
+	return {init};
+	
 }());
+
+document.addEventListener("DOMContentLoaded", Details.init);
